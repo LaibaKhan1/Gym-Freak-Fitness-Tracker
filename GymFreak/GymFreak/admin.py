9eeb8adbe13fe122bb09e13d9d1c821ca8591d73
SyedHassanUlHaq <21b-029-cs@students.uit.edu>
2024-01-06 20:21:57 +0500

    initiated exercise display page

--- GymFreak/GymFreak/admin.py
+++ GymFreak/GymFreak/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import ex_routines, Orders, OrderUpdate
+from .models import ex_routines
 
 # Register your models here.
 admin.site.register(ex_routines)
-admin.site.register(Orders)
-admin.site.register(OrderUpdate)
+# admin.site.register(Product)
+# admin.site.register(Orders)
+# admin.site.register(OrderUpdate)
