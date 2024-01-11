a3c11517df36366c9e3f9da3af68f02f4fc36408
SyedHassanUlHaq <21b-029-cs@students.uit.edu>
2024-01-12 01:05:27 +0500

    implemented calories burned in views.py

--- GymFreak/GymFreak/forms.py
+++ GymFreak/GymFreak/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import CalorieEntry
+from .models import CalorieEntry, CaloriesBurned
 
 class CalorieEntryForm(forms.ModelForm):
     class Meta:
         model = CalorieEntry
         fields = ['calories_consumed', 'time', 'date']
+        
+class CalorieBurnedEntryForm(forms.ModelForm):
+    class Meta:
+        model = CaloriesBurned
+        fields = ['time', 'date', 'weight', 'age', 'gender', 'exercise_type', 'duration_minutes']
